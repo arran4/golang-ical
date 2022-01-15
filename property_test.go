41a5700e4fe072fdb4ae68a31adba74066692759
Arran Ubels <arran4@gmail.com>
2022-01-15 17:02:01 +1100

    Sub test run. Also added x-prop test.

--- property_test.go
+++ property_test.go
@@ -8,19 +8,25 @@ import (
 
 func TestPropertyParse(t *testing.T) {
 	tests := []struct {
+		Name     string
 		Input    string
 		Expected func(output *BaseProperty) bool
 	}{
-		{Input: "ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool {
+		{Name: "ATTENDEE1", Input: "ATTENDEE;RSVP=TRUE;ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool {
 			return output.IANAToken == "ATTENDEE" && output.Value == "mailto:employee-A@example.com"
 		}},
-		{Input: "ATTENDEE;RSVP=\"TRUE\";ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool {
+		{Name: "ATTENDEE2", Input: "ATTENDEE;RSVP=\"TRUE\";ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool {
 			return output.IANAToken == "ATTENDEE" && output.Value == "mailto:employee-A@example.com"
 		}},
-		{Input: "ATTENDEE;RSVP=T\"RUE\";ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool { return output == nil }},
+		{Name: "ATTENDEE - fail", Input: "ATTENDEE;RSVP=T\"RUE\";ROLE=REQ-PARTICIPANT;CUTYPE=GROUP:mailto:employee-A@example.com", Expected: func(output *BaseProperty) bool { return output == nil }},
+		{Name: "X-ABC-MMSUBJ custom arg", Input: "X-ABC-MMSUBJ;VALUE=URI;FMTTYPE=audio/basic:http://www.example.org/mysubj.au", Expected: func(output *BaseProperty) bool {
+			return output.IANAToken == "X-ABC-MMSUBJ" && output.Value == "audio/basic:http://www.example.org/mysubj.au"
+		}},
 	}
 	for _, test := range tests {
-		output := ParseProperty(ContentLine(test.Input))
-		assert.True(t, test.Expected(output))
+		t.Run(test.Name, func(t *testing.T) {
+			output := ParseProperty(ContentLine(test.Input))
+			assert.True(t, test.Expected(output))
+		})
 	}
 }
